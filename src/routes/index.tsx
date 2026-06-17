841ed64908a4f927aa7aeae84f2d44fbed78d893
Joel Sharon <97514566+jsjoel@users.noreply.github.com>
2026-06-17 19:31:20 +0800

    Fix syntax error in index route and add missing router.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/'){
+export const Route = createFileRoute('/')({ 
   component: Index,
-}
+})
 
 function Index() {
   return (
